7ef339915016707052802ebc5007be09922e4d06
jmurtozoev <m.jasurbek@inbox.ru>
2021-06-12 03:25:45 +0500

    added personalization

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/lhhong/go-fcm/fcm"
 	"log"
@@ -9,7 +10,10 @@ import (
 	"strconv"
 )
 
+var cache []Scarf
+
 func init()  {
+	cache = make([]Scarf, 0)
 	if err := ConnectDb(""); err != nil {
 		panic(err)
 	}
@@ -17,7 +21,7 @@ func init()  {
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/index.tmpl")
+	router.LoadHTMLGlob("templates/*.tmpl")
 
 	router.GET("/", func(c *gin.Context) {
 		scarves, err := GetAll()
@@ -39,6 +43,7 @@ func main() {
 	})
 
 	router.GET("/search", filterScarves)
+	router.GET("/personalize", personalize)
 
 	log.Fatal(router.Run(":8080"))
 
@@ -47,6 +52,7 @@ func main() {
 
 func filterScarves(c *gin.Context) {
 	var scarves []Scarf
+	var lengthOption, widthOption, priceOption int
 	var filtersCount int
 	var priceCluster, widthCluster, lengthCluster map[int]float64
 	results := make([]Result, 0)
@@ -55,6 +61,134 @@ func filterScarves(c *gin.Context) {
 	if err != nil {
 		log.Println("Error getting scarves list")
 	}
+
+	if priceOption, err = strconv.Atoi(c.Query("price")); err == nil && priceOption > 1 {
+		value := "min"
+		switch priceOption {
+		case 3:
+			value = "avg"
+		case 4:
+			value = "max"
+		}
+
+		fcmPoints := make([]fcm.Interface, len(scarves))
+		for i, s := range scarves {
+			fcmPoints[i] = FcmPoint(Point{
+				float64(s.Id),
+				s.Price,
+			})
+		}
+		priceCluster = FindFCM(fcmPoints, value)
+		filtersCount++
+	}
+
+
+	if widthOption, err = strconv.Atoi(c.Query("width")); err == nil && widthOption > 1 {
+		value := "min"
+		switch widthOption {
+		case 3:
+			value = "avg"
+		case 4:
+			value = "max"
+		}
+
+		fcmPoints := make([]fcm.Interface, len(scarves))
+		for i, s := range scarves {
+			fcmPoints[i] = FcmPoint(Point{
+				float64(s.Id),
+				float64(s.Width),
+			})
+		}
+		widthCluster = FindFCM(fcmPoints, value)
+		filtersCount++
+	}
+
+	if lengthOption, err = strconv.Atoi(c.Query("length")); err == nil && lengthOption > 1 {
+		value := "min"
+		switch lengthOption {
+		case 3:
+			value = "avg"
+		case 4:
+			value = "max"
+		}
+
+		fcmPoints := make([]fcm.Interface, len(scarves))
+		for i, s := range scarves {
+			fcmPoints[i] = FcmPoint(Point{
+				float64(s.Id),
+				float64(s.Length),
+			})
+		}
+		lengthCluster = FindFCM(fcmPoints, value)
+		filtersCount++
+	}
+
+	for _, s := range scarves {
+		result := Result{
+			Scarf: s,
+			TotalWeight: 1,
+		}
+
+		if v, ok := priceCluster[s.Id]; ok {
+			result.TotalWeight *= v
+		}
+
+		if v, ok := lengthCluster[s.Id]; ok {
+			result.TotalWeight *= v
+		}
+
+		if v, ok := widthCluster[s.Id]; ok {
+			result.TotalWeight *= v
+		}
+
+		if filtersCount == 1 && result.TotalWeight > 0.5 {
+			results = append(results, result)
+		} else if filtersCount == 2 && result.TotalWeight > 0.25 {
+			results = append(results, result)
+		} else if filtersCount == 3 && result.TotalWeight > 0.1 {
+			results = append(results, result)
+		} else if filtersCount == 0 {
+			results = append(results, result)
+		}
+	}
+
+	sort.Slice(results, func(i, j int) bool { return results[i].TotalWeight > results[j].TotalWeight})
+
+	updatedScarves := make([]Scarf, 0)
+	for _, r := range results {
+		updatedScarves = append(updatedScarves, r.Scarf)
+	}
+
+	if len(results) >= 3 {
+		c.HTML(http.StatusOK, "personalization", map[string]interface{}{
+			"SelectedPrice": priceOption,
+			"SelectedLength": lengthOption,
+			"SelectedWidth": widthOption,
+			"Price": priceOptions,
+			"Length": lengthOptions,
+			"Width": widthOptions,
+			"Rows": results,
+		})
+
+		// set sorted scarves into cache
+		cache = updatedScarves
+		copy(cache, updatedScarves)
+		fmt.Println(len(cache), cap(cache), len(updatedScarves))
+	} else {
+		c.HTML(http.StatusOK, "final", map[string]interface{}{
+			"Rows": results,
+		})
+	}
+}
+
+func personalize(c *gin.Context) {
+	var scarves []Scarf
+	var filtersCount int
+	var priceCluster, widthCluster, lengthCluster map[int]float64
+	results := make([]Result, 0)
+
+	scarves = cache
+	fmt.Println(len(cache))
 
 	if priceOption, err := strconv.Atoi(c.Query("price")); err == nil && priceOption > 1 {
 		value := "min"
@@ -148,15 +282,10 @@ func filterScarves(c *gin.Context) {
 
 	sort.Slice(results, func(i, j int) bool { return results[i].TotalWeight > results[j].TotalWeight})
 
-	updatedScarves := make([]Scarf, 0)
-	for _, r := range results {
-		updatedScarves = append(updatedScarves, r.Scarf)
-	}
-
-	c.HTML(http.StatusOK, "main", map[string]interface{}{
-		"Price": priceOptions,
-		"Length": lengthOptions,
-		"Width": widthOptions,
+	// empty cache
+	cache = make([]Scarf, 0)
+	c.HTML(http.StatusOK, "final", map[string]interface{}{
 		"Rows": results,
 	})
+
 }
